b55cb7ec54f77ccb51fad8cca0e09dc13aac869d
“Suncake” <xexc@mail.ru>
2020-10-22 16:21:30 +0500

    Create trackers table if it doesn't exist

--- bot/utils/settings.py
+++ bot/utils/settings.py
@@ -4,21 +4,25 @@ async def read_settings(connection_pool):
     """Read bot settings from the database. 
     Create missing tables if there are any."""
     settings = dict()
-    async with connection_pool.acquire() as connection:
-        async with connection.transaction():
-            print(await connection.execute("\dt"))
-            # Fetch tracker settings
-            try:
-                result = await connection.fetch(
-                    "SELECT * FROM trackers"
-                )
-            except UndefinedTableError:
-                await connection.execute(
-                    """CREATE TABLE trackers (
-                        guild_id biginteger PRIMARY KEY REFERENCES guilds (guild_id)
-                    )"""
-                )
-                result = {}
-            print(result)
+    try:
+        async with connection_pool.acquire() as connection:
+            async with connection.transaction():
+                print(await connection.execute("\dt"))
+                # Fetch tracker settings
+                try:
+                    result = await connection.fetch(
+                        "SELECT * FROM trackers"
+                    )
+                except UndefinedTableError:
+                    await connection.execute(
+                        "CREATE TABLE trackers ("
+                            "guild_id biginteger PRIMARY KEY REFERENCES guilds (guild_id)"
+                        ")"
+                    )
+                    result = {}
+                print(result)
+    except Exception as e:
+        print(e)
+        print(type(e))
 
     return settings
