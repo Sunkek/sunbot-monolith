b3d10a653f84eddc6c27ba28983e509f30ccf6c7
“Suncake” <xexc@mail.ru>
2020-11-18 16:33:10 +0500

    remove debug print

--- bot/cogs/trackers/track_messages.py
+++ bot/cogs/trackers/track_messages.py
@@ -14,52 +14,45 @@ class TrackMessages(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         # I don't want to save info about DMs and webhooks
-        try:
-            if message.guild and message.guild.get_member(message.author.id) and \
-                not message.author.bot:
-                
-                if self.bot.settings.get(message.guild.id, {})\
-                    .get("track_messages", False):
+        if message.guild and message.guild.get_member(message.author.id) and \
+            not message.author.bot:
+            
+            if self.bot.settings.get(message.guild.id, {})\
+                .get("track_messages", False):
 
-                    await util_trackers.add_message(
-                        self.bot, 
-                        guild_id=message.guild.id,
-                        channel_id=message.channel.id,
-                        user_id=message.author.id,
-                        postcount=1,
-                        attachments=len(message.attachments),
-                        words=len(message.content.split()),
-                        period=date.today()
-                    )   
+                await util_trackers.add_message(
+                    self.bot, 
+                    guild_id=message.guild.id,
+                    channel_id=message.channel.id,
+                    user_id=message.author.id,
+                    postcount=1,
+                    attachments=len(message.attachments),
+                    words=len(message.content.split()),
+                    period=date.today()
+                )   
 
-                # Add activity points, if set
-                min_words = self.bot.settings.get(message.guild.id, {})\
-                    .get("activity_min_message_words", 0) 
-                words = len(message.content.split())
-                print(min_words, words)
-                if words < min_words:
-                    return
-                per_word = self.bot.settings.get(message.guild.id, {})\
-                    .get("activity_multi_per_word", 1) 
-                per_message = self.bot.settings.get(message.guild.id, {})\
-                    .get("activity_per_message", 0) 
-                per_attachment = self.bot.settings.get(message.guild.id, {})\
-                    .get("activity_per_attachment", 0) 
-                from_text = per_message * per_word**min(words-min_words, 100)
-                print(from_text)
-                from_attachments = per_attachment * len(message.attachments)
-                print(from_attachments)
-                if from_text or from_attachments:
-                    await util_trackers.add_activity(
-                        self.bot, 
-                        channel_id=message.channel.id,
-                        from_text=from_text,
-                        from_attachments="from_attachments",
-                        period=date.today()
-                    )   
-        except Exception as e:
-            print(e)
-            print(type(e))
+            # Add activity points, if set
+            min_words = self.bot.settings.get(message.guild.id, {})\
+                .get("activity_min_message_words", 0) 
+            words = len(message.content.split())
+            if words < min_words:
+                return
+            per_word = self.bot.settings.get(message.guild.id, {})\
+                .get("activity_multi_per_word", 1) 
+            per_message = self.bot.settings.get(message.guild.id, {})\
+                .get("activity_per_message", 0) 
+            per_attachment = self.bot.settings.get(message.guild.id, {})\
+                .get("activity_per_attachment", 0) 
+            from_text = per_message * per_word**min(words-min_words, 100)
+            from_attachments = per_attachment * len(message.attachments)
+            if from_text or from_attachments:
+                await util_trackers.add_activity(
+                    self.bot, 
+                    channel_id=message.channel.id,
+                    from_text=from_text,
+                    from_attachments=from_attachments,
+                    period=date.today()
+                )   
 
 def setup(bot):
     bot.add_cog(TrackMessages(bot))
