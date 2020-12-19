e916547b004ce97aa22578bee51b1b58293d7012
Sunkek <xexc@mail.ru>
2020-12-20 02:28:13 +0500

    Send the DM to the gifters

--- bot/cogs/fun/santa.py
+++ bot/cogs/fun/santa.py
@@ -20,50 +20,47 @@ class SecretSanta(commands.Cog):
         channel: discord.TextChannel, 
         message_id: int,
     ):
-        try:
-            # Make a list of participants
-            message = await channel.fetch_message(message_id)
-            givers = []
-            for reaction in message.reactions:
-                if reaction.emoji == "🎅":
-                    givers = await reaction.users().flatten()
-                    break
-            if len(givers) < 2:
-                raise commands.BadArgument
-            # Make pairs of givers and receivers
-            receivers = givers.copy()
-            while any([g == r for g, r in zip(givers, receivers)]):
-                shuffle(receivers)
-            # Send DMs to the givers with their receivers
-            link = (
-                f"https://discordapp.com/channels/"
-                f"{ctx.guild.id}/{channel.id}/{message_id}"
+        # Make a list of participants
+        message = await channel.fetch_message(message_id)
+        givers = []
+        for reaction in message.reactions:
+            if reaction.emoji == "🎅":
+                givers = await reaction.users().flatten()
+                break
+        if len(givers) < 2:
+            raise commands.BadArgument
+        # Make pairs of givers and receivers
+        receivers = givers.copy()
+        while any([g == r for g, r in zip(givers, receivers)]):
+            shuffle(receivers)
+        # Send DMs to the givers with their receivers
+        link = (
+            f"https://discordapp.com/channels/"
+            f"{ctx.guild.id}/{channel.id}/{message_id}"
+        )
+        for g, r in zip(givers, receivers):
+            desc = (
+                f"You have been assigned to be a secret Santa "
+                f"for {r.mention} ({r.display_name}) "
+                f"because you reacted [here]({link})!"
             )
-            for g, r in zip(givers, receivers):
-                desc = (
-                    f"You have been assigned to be a secret Santa "
-                    f"for {r.mention} ({r.display_name}) on {ctx.guild.name} "
-                    f"because you reacted [here]({link})!"
-                )
-                e = discord.Embed(
-                    title=f"Secret Santa on {ctx.guild.name}",
-                    description=desc,
-                    color=discord.Colour.red(),
-                )
-                await ctx.author.send(embed=e)  # Change it to DM givers!
-            # Send a list of pairs to the command invoke channel
-            desc = "\n".join([
-                f"{g.mention} - {r.mention}" 
-                for g, r in zip(givers, receivers)
-            ])
             e = discord.Embed(
-                title="Secret Santa Pairs",
-                color=ctx.author.color,
-                description=desc
+                title=f"Secret Santa on {ctx.guild.name}",
+                description=desc,
+                color=discord.Colour.red(),
             )
-            await ctx.send(embed=e)
-        except Exception as e:
-            print(e)
+            await g.send(embed=e)
+        # Send a list of pairs to the command invoke channel
+        desc = "\n".join([
+            f"{g.mention} - {r.mention}" 
+            for g, r in zip(givers, receivers)
+        ])
+        e = discord.Embed(
+            title="Secret Santa Pairs",
+            color=ctx.author.color,
+            description=desc
+        )
+        await ctx.send(embed=e)
 
 
 def setup(bot):
