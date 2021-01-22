67058f52bb5acbeab2c0c030da16515fac6f4198
Sunkek <xexc@mail.ru>
2021-01-23 00:13:09 +0500

    Convert zipped list to list

--- bot/cogs/votes.py
+++ bot/cogs/votes.py
@@ -129,7 +129,7 @@ class Votes(commands.Cog):
                         self.bot, guild.id, m.id
                     ) for m in members
                 ]
-                table = zip(activities, [m.mention for m in members])
+                table = list(zip(activities, [m.mention for m in members]))
                 table = utils.format_columns(
                     sorted(table, key=lambda t: -t[0]), 
                     headers=("ACTIVITY", "MEMBER")
