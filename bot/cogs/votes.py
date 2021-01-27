18fe765dbf7273c2f69b428ecfdf015e9b46088b
Sunkek <xexc@mail.ru>
2021-01-27 22:49:07 +0500

    Send candidates lists to voters

--- bot/cogs/votes.py
+++ bot/cogs/votes.py
@@ -13,33 +13,13 @@ from utils import util_users, util_user_stats, utils
 class Votes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.numbers = {
-            1:':1icon:658633333051228161', 2:':2icon:658633333692956673',
-            3:':3icon:658633334255124480', 4:':4icon:658633333625847809',
-            5:':5icon:658633333877637130', 6:':6icon:658633334217244682',
-            7:':7icon:658633334288678922', 8:':8icon:658633334234152980',
-            9:':9icon:658633334234021898', 10:':10icon:658633333919711235',
-            11:':11icon:658633333395423242', 12:':12icon:658633334158524417',
-            13:':13icon:658633334028763137', 14:':14icon:658633333688893444',
-            15:':15icon:658633334171238410', 16:':16icon:658633334049734677',
-            17:':17icon:658633333688893464', 18:':18icon:658633334141747200',
-            19:':19icon:658633334183821322', 20:':20icon:658633334338879518',
-            21:':21icon:658633334192341022', 22:':22icon:658633333911322624',
-            23:':23icon:658633334036889613', 24:':24icon:658633334167175179',
-            25:':25icon:658633334250930189', 26:':26icon:658633334234152981',
-            27:':27icon:658633334556983317', 28:':28icon:658633334704046080',
-            29:':29icon:658633334263644161', 30:':30icon:658633334548725780',
-            31:':31icon:658633334456451082', 32:':32icon:658633334418702357',
-            33:':33icon:658633334284484619', 34:':34icon:658633334540468244',
-            35:':35icon:658633334695395351', 36:':36icon:658633334506913806',
-            37:':37icon:658633334473228288', 38:':38icon:658633334208856085',
-            39:':39icon:658633334347268137', 40:':40icon:658633334368370700',
-            41:':41icon:658633334439542785', 42:':42icon:658633334490136587',
-            43:':43icon:658633334473359370', 44:':44icon:658633334527623168',
-            45:':45icon:658633334548856844', 46:':46icon:658633334489874462',
-            47:':47icon:658633334527754260', 48:':48icon:658633334787932180',
-            49:':49icon:658633334422896651', 50:':50icon:658633334288678913',
-        }
+        self.numbers = [
+            ':1icon:658633333051228161', ':2icon:658633333692956673',':3icon:658633334255124480', ':4icon:658633333625847809',':5icon:658633333877637130', ':6icon:658633334217244682',':7icon:658633334288678922', ':8icon:658633334234152980',':9icon:658633334234021898', 
+            ':10icon:658633333919711235',':11icon:658633333395423242', ':12icon:658633334158524417',':13icon:658633334028763137', ':14icon:658633333688893444',':15icon:658633334171238410', ':16icon:658633334049734677',':17icon:658633333688893464', ':18icon:658633334141747200',':19icon:658633334183821322', 
+            ':20icon:658633334338879518',':21icon:658633334192341022', ':22icon:658633333911322624',':23icon:658633334036889613', ':24icon:658633334167175179',':25icon:658633334250930189', ':26icon:658633334234152981',':27icon:658633334556983317', ':28icon:658633334704046080', ':29icon:658633334263644161', 
+            ':30icon:658633334548725780', ':31icon:658633334456451082', ':32icon:658633334418702357', ':33icon:658633334284484619', ':34icon:658633334540468244', ':35icon:658633334695395351', ':36icon:658633334506913806', ':37icon:658633334473228288', ':38icon:658633334208856085', ':39icon:658633334347268137', 
+            ':40icon:658633334368370700', ':41icon:658633334439542785', ':42icon:658633334490136587',':43icon:658633334473359370', ':44icon:658633334527623168',':45icon:658633334548856844', ':46icon:658633334489874462',':47icon:658633334527754260', ':48icon:658633334787932180',':49icon:658633334422896651', ':50icon:658633334288678913',
+        ]
         self.votes.start()
 
     def cog_unload(self):
@@ -55,7 +35,9 @@ class Votes(commands.Cog):
         message = await channel.fetch_message(payload.message_id)
         voter = guild.get_member(payload.user_id)
         settings = self.bot.settings.get(guild.id)
-        if message.author != guild.me or voter.bot:
+        if message.embeds and \
+            "Junior mod vote start" not in message.embeds[0].title or \
+            message.author != guild.me or voter.bot:
             return
         now = datetime.now()
         junior_mod_vote_day = settings.get("vote_junior_mod_day")
@@ -76,6 +58,7 @@ class Votes(commands.Cog):
             now.month in admin_vote_months,
         )):
             await message.remove_reaction('☑️', voter)
+            #return
         active_member = guild.get_role(settings.get("rank_active_member_role_id"))
         junior_mod = guild.get_role(settings.get("rank_junior_mod_role_id"))
         senior_mod = guild.get_role(settings.get("rank_senior_mod_role_id"))
@@ -83,6 +66,7 @@ class Votes(commands.Cog):
             junior_mod not in voter.roles and \
             senior_mod not in voter.roles:
             await message.remove_reaction('☑️', voter)
+            #return
         # Fetch the candidate list from the vote start message
         raw_candidates = message.embeds[0].description.split("\n")[1:]
         raw_candidates = [i.split("<")[1] for i in raw_candidates]
@@ -90,10 +74,31 @@ class Votes(commands.Cog):
         for m in raw_candidates:
             m_id = sub("[^0-9]", "", m)
             m = guild.get_member(m_id) or await self.bot.fetch_user(m_id)
-            print(m.display_name)
-
+            candidates += m
+        print([m.display_name for m in candidates])
         # Build new embed(s) and send it(them) to the voter
-
+        for num, candidate in enumerate(candidates):
+            candidates[num] = f"{self.numbers[num]} {candidate}"
+        desc_embed = discord.Embed(
+            title=message.embeds[0].title + " on " + guild.name,
+            color=guild.me.color
+        )
+        desc_embed.description = f"React to the messages below with candidate numbers to vote for them.\n\n**Important** - If you're among the candidates, but don't want the promotion - don't upvote yourself. If the embed misses some reactions, rereact to the initial [vote message]({message.jump_url}) on server."
+        embeds = []
+        for i in range(len(candidates)//20):
+            embed = discord.Embed(
+                title=message.embeds[0].title + " on " + guild.name + " candidates",
+                description="\n".join(candidates[i*20:(i+1)*20]),
+                color=guild.me.color
+            )
+            embeds += embed
+        # Sending embeds and adding reactions to them
+        await voter.send(embed=desc_embed)
+        for num, embed in enumerate(embeds):
+            msg = await voter.send(embed=embed)
+            for number in range(num*20, min((num+1)*20, len(candidates))):
+                await msg.add_reaction(self.numbers[number])
+        
     @tasks.loop(hours=24.0)
     async def votes(self):
         """Run the votes for server staff"""
